30f1ebebd4943a3cf20491278064e5d281609c6f
Samuel <fernandesamuel2002@gmail.com>
2025-10-26 17:05:09 +0530

    Background color changer created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
+  const [color, setColor] = useState("black");
 
   return (
     <>
-      <h1>Samuel | React with vite</h1>
+      <div className="w-full h-screen" style={{ backgroundColor: color }}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "Red" }} onClick={() => setColor("red")}>
+              Red
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "green" }} onClick={() => setColor("green")}>
+              Green
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "blue" }} onClick={() => setColor("blue")}>
+              Blue
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{ backgroundColor: "pink" }} onClick={() => setColor("pink")}>
+              Pink
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{ backgroundColor: "yellow" }} onClick={() => setColor("yellow")}>
+              Yellow
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "black" }} onClick={() => setColor("black")}>
+              Black
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
